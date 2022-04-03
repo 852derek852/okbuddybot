178b76dc3849176ec0092c3f8a1bf78f6bd65949
852derek852 <102877700+852derek852@users.noreply.github.com>
2022-04-02 20:38:46 -0400

    Update startokbuddybot.sh

--- startokbuddybot.sh
+++ startokbuddybot.sh
@@ -1,5 +1,5 @@
 rm -rf reddit-place-script-2022
-git clone https://github.com/rdeepak2002/reddit-place-script-2022.git
-cp image.png config.json reddit-place-script-2022
-cd reddit-place-script-2022
+git clone https://github.com/rdeepak2002/reddit-place-script-2022.git &&
+cp image.png config.json reddit-place-script-2022 &&
+cd reddit-place-script-2022 &&
 ./start.sh
